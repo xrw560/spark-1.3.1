cdc0eaa412c9f82b13f66f3def2ec1aba28da4a1
周宁 <zhouning@yzhtech.com>
2017-09-22 12:28:27 +0800

    no message

---
+++ core/src/main/scala/cn/itcast/spark/rdd/WordCount.scala
@@ -0,0 +1,21 @@
+package cn.itcast.spark.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by root on 2016/5/27.
+  */
+object WordCount {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("WordCount")
+    val sc = new SparkContext()
+    //构建RDD并调用了他的Transformation
+    val wordAndCount: RDD[(String, Int)] = sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_)
+    //TODO 调用RDD的Action开始真正提交任务
+    wordAndCount.saveAsTextFile(args(1))
+    //关闭SparkContext释放资源
+    sc.stop()
+  }
+}
